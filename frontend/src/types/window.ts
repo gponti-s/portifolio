d9013b2496ff0e9ae850d72eca4efa247c9447bd
Guilherme Seletti <guiseletti@gmail.com>
2024-12-19 22:30:03 +0100

    frontend - builded version

--- frontend/src/types/window.ts
+++ frontend/src/types/window.ts
@@ -1,7 +1,7 @@
-declare module 'global' {
-    global {
-        interface Window {
-            ethereum: any; // or a more specific type if known
-        }
+export {}; 
+// Corrected declaration for extending the global Window interface
+declare global {
+    interface Window {
+        ethereum: any; // or a more specific type if known
     }
 }
