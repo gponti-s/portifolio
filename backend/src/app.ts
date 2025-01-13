248fddbf6c6f0fb9a50eb67153f4832a4696ac24
Guilherme <guiseletti@gmail.com>
2025-01-13 10:24:43 +0100

    backend - delete index.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,50 @@
-import app from '.';
+import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import userController from './controllers/userController.js';
+import cors from 'cors';
+import initializeDatabase from './config/database.js';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 dotenv.config();
 
+const app = express();
 const port = process.env.PORT || 5001;
 
-// Server
+// Middleware
+app.use(express.json());
+
+const corsOptions = {
+  origin: process.env.NODE_ENV === 'production' 
+    ? 'https://portifolio-rose-nu.vercel.app'
+    : 'http://localhost:3000',
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+app.use(helmet());
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+}));
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI as string)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('MongoDB connection error:', error));
+
+initializeDatabase();
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('API is working!');
+});
+
+app.use('/api/user', userController);
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+export default app;
